585a5c7e6e7fa071722cfa2b020b89c133a50572
Guro <queengurp@gmail.com>
2023-09-27 23:31:48 +0200

    Error encountered when joinin a game with a code

--- frontend/my-app/src/Game/GameRoom.js
+++ frontend/my-app/src/Game/GameRoom.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Components/Header";
 import RecieveCodeComponent from "../Components/RecieveCodeComponent";
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 export default function GameRoom() {
+    const { gameCode } = useParams();
+    const [players, setPlayers] = useState([]);
+    const isRoomOwner = !gameCode; // If gameCode is not in URL, this user is the room owner
+
+    useEffect(() => {
+        if (gameCode) {
+          // Fetch the game room details and set the players list
+          axios.get(`http://localhost:8080/api/v1/gameroom/gameRoom/code/${gameCode}`)
+            .then(response => {
+              // Assuming the response contains a list of players
+              setPlayers(response.data.players); // Update the players list
+            })
+            .catch(error => {
+              console.error("Error fetching game room details:", error);
+            });
+        }
+      }, [gameCode]);
+
 return(
     <>
     <Header />
-    <div className="gameroom-container">
-        
-        <div className="code-container">
-            <RecieveCodeComponent />
+    <div>
+            {isRoomOwner ? <RecieveCodeComponent /> : <h2>Game Code: {gameCode}</h2>}
+            <h3>Players in Room:</h3>
+            <ul>
+                {players.map(player => (
+                <li key={player.id}>{player.name}</li>
+                ))}
+            </ul>
         </div>
-    </div>
     </>
     
 );
--- frontend/my-app/src/Pages/JoinGame.js
+++ frontend/my-app/src/Pages/JoinGame.js
@@ -1,37 +1,51 @@
-import {React, useState} from "react";
+import React, {useState} from "react";
 import Header from "../Components/Header";
 import { Link } from 'react-router-dom';
 import './JoinGame.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function JoinGame() {
     
     const [nickname, setNickname] = useState('');
     const [gameCode, setGameCode] = useState('');
+    const navigate = useNavigate();
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Nickname:', nickname);
-        console.log('Game Code:', gameCode);
+        // Making API call to join the game
+        axios.post('http://localhost:8080/api/v1/gameroom/join', {
+            nickname,
+            gameCode,
+        })
+        .then(response => {
+            // On successful join, navigate to the game room
+            navigate.push(`/gameRoom/${gameCode}`);
+        })
+        .catch(error => {
+            console.error("Error joining game room:", error);
+        });
     };
 
-    return(<div className="joingame-container">
-        <Header />
-    <h1>Join Game Page</h1>
-    <form onSubmit={handleSubmit}>
-    <div className="nickname-container">
-    <label htmlFor="nickname">Enter nickname: </label>
-        <input type="text" id="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} placeholder="Enter your nickname" />
-    </div>
-    <div className="code-container">
-        <label htmlFor="gamecode">Enter game code: </label>
-        <input type="text" id="gamecode" value={gameCode} onChange={(e) => setGameCode(e.target.value)} placeholder="Enter the game code" />
-    </div>
-    <div className="enter-button-container">
-        <Link className='enter-btn' to={`/gameRoom/${gameCode}`}> Enter game </ Link>
-    </div>
-    
-    </form>
-    </div>
+    return(
+        <div className="joingame-container">
+            <Header />
+            <h1>Join Game Page</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="nickname-container">
+                    <label htmlFor="nickname">Enter nickname: </label>
+                    <input type="text" id="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} placeholder="Enter your nickname" />
+                </div>
+                <div className="code-container">
+                    <label htmlFor="gamecode">Enter game code: </label>
+                    <input type="text" id="gamecode" value={gameCode} onChange={(e) => setGameCode(e.target.value)} placeholder="Enter the game code" />
+                </div>
+                <div className="enter-button-container">
+                    <Link className='enter-btn' to={`/gameRoom/${gameCode}`}> Enter game </ Link>
+                </div>
+            </form>
+        </div>
     );
     
     
